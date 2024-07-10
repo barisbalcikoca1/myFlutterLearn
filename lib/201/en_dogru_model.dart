0eb5919a40dfc3137020e72fa7c6f02d93b27e16
barisbalcikoca1 <barisbalcikoca@hotmail.com>
2024-07-10 11:53:26 +0300

    en doğru veri modeli ve paket ekleme

---
+++ lib/201/en_dogru_model.dart
@@ -0,0 +1,11 @@
+class Model {
+  final int? userId;
+  final int? id;
+  final String? title;
+  final String? body;
+
+  Model({this.userId, this.id, this.title, this.body});
+}
+
+//Servisten data geliyorsa
+//internetten gelecek değerlerin karşılanmasında en doğru model bu
